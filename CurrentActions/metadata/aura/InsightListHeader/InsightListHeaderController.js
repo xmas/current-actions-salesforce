2de83ffcfb55f33c1830aa2018b22c5793551fa9
Rowan Christmas <rowanxmas@gmail.com>
2016-03-23 10:47:56 -0700

    add stats on data being processed

--- CurrentActions/metadata/aura/InsightListHeader/InsightListHeaderController.js
+++ CurrentActions/metadata/aura/InsightListHeader/InsightListHeaderController.js
@@ -2,7 +2,7 @@
 	
 	doInit : function(component, event, helper) {
 
-         var action = component.get("c.getUserName");
+     var action = component.get("c.getUserName");
 
         //Set up the callback
         var self = this;
@@ -13,9 +13,24 @@
 
         });
         $A.enqueueAction(action);
-	},
-	
-	postScript : function(component, event, helper) {
+
+        var data_action = component.get("c.getInsightCounts");
+        data_action.setCallback(this, function(actionResult) {
+
+            var result = JSON.parse(actionResult.getReturnValue()); 
+
+            component.set("v.global_total", result.all_users.total);
+            component.set("v.global_new_changed", result.all_users.changed+result.all_users.new);
+            component.set("v.user_total", result.user.total);
+            component.set("v.user_new_changed", result.user.changed+result.user.new);
+
+        });
+        $A.enqueueAction(data_action);
+
+
+    },
+
+    postScript : function(component, event, helper) {
 
 		//var AWS;
 
