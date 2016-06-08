7a2ffb70357232fa7a66fb9d145b7117e4c5a2ea
Rowan Christmas <rowanxmas@gmail.com>
2016-06-07 23:48:50 -0700

    remove color changer listener on slide_out

--- CurrentActions/metadata/aura/MInsightList/MInsightListController.js
+++ CurrentActions/metadata/aura/MInsightList/MInsightListController.js
@@ -13,12 +13,18 @@
         var x = document.getElementsByClassName("toggleNav")[0];
         var sf1_color = x.style.color;
         component.set("v.sf1_color", sf1_color);
-        x.addEventListener("click", function(){
-          if (component.isValid()) {
+
+        var remove_color_changer = function () {
+          x.removeEventListener("click", color_changer);
+        };
+
+        var color_changer = function(){
             oneHeader.style.backgroundColor = component.get("v.sf1_color");
             document.getElementById('oneHeader').style.transition = 0.5;
-          }
-        });
+            remove_color_changer();
+        };
+
+        x.addEventListener("click", color_changer);
   },
 
   postScript: function(component, event, helper) {
