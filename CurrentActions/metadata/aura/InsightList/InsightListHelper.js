0b1dee4a34d587011b4cc4bc162d7269af09f04c
Rowan Christmas <rowanxmas@gmail.com>
2016-06-22 15:27:19 -0700

    dead code

--- CurrentActions/metadata/aura/InsightList/InsightListHelper.js
+++ CurrentActions/metadata/aura/InsightList/InsightListHelper.js
@@ -5,14 +5,9 @@
 
         //Set up the callback
         var self = this;
-        action.setCallback(this, function(actionResult) {
+        action.setCallback(self, function(actionResult) {
             component.set("v.insights", actionResult.getReturnValue());
-
             window.scrollTo(0,0);
-            //$("#ticket-event-list").accordion('activate', 0 );
-            //jQuery(".selector").accordion("option", {active: 0});
-
-
         });
         $A.enqueueAction(action);
     },
@@ -26,7 +21,7 @@
 
         //Set up the callback
         var self = this;
-        action.setCallback(this, function(actionResult) {
+        action.setCallback(self, function(actionResult) {
             component.set("v.insights", actionResult.getReturnValue());
             window.scrollTo(0,0);
 
@@ -46,7 +41,7 @@
 
         //Set up the callback
         var self = this;
-        action.setCallback(this, function(actionResult) {
+        action.setCallback(self, function(actionResult) {
 
             var result = actionResult.getReturnValue();
             console.log("setting new insights");
@@ -72,7 +67,7 @@
 
         //Set up the callback
         var self = this;
-        action.setCallback(this, function(actionResult) {
+        action.setCallback(self, function(actionResult) {
             component.set("v.insights", actionResult.getReturnValue());
             window.scrollTo(0,0);
 
@@ -110,106 +105,6 @@
         selectEvent.setParams({ "insight": insight });
         selectEvent.fire();
 
-    },
-
-    registerServiceWorker : function() {
-
-        console.log('REGISTER A SERVICE WORKER');
-
-        var isPushEnabled = false;
-
-
-
-//            console.log('load event listener');
-       //     var pushButton = document.querySelector('.js-push-button');
-         //   pushButton.addEventListener('click', function() {
-        //        if (isPushEnabled) {
-       //             unsubscribe();
-         //       } else {
-                   // subscribe();
-         //       }
-//});
-
-            // Check that service workers are supported, if so, progressively
-            // enhance and add push messaging support, otherwise continue without it.
-            if ('serviceWorker' in navigator) {
-                console.log('REGISTER SERVICE WORKER JS????');
-                navigator.serviceWorker.register('/service-worker.js')
-                .then(this.initialiseState());
-            } else {
-                console.log('Service workers aren\'t supported in this browser.');
-            }
-        },
-
-    // Once the service worker is registered set the initial state
-    initialiseState : function () {
-        // Are Notifications supported in the service worker?
-        if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
-            console.warn('Notifications aren\'t supported.');
-            return;
-        }
-
-        // Check the current Notification permission.
-        // If its denied, it's a permanent block until the
-        // user changes the permission
-        if (Notification.permission === 'denied') {
-            console.warn('The user has blocked notifications.');
-            return;
-        }
-
-        // Check if push messaging is supported
-        if (!('PushManager' in window)) {
-            console.warn('Push messaging isn\'t supported.');
-            return;
-        }
-
-        // We need the service worker registration to check for a subscription
-        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-            // Do we already have a push message subscription?
-            serviceWorkerRegistration.pushManager.getSubscription()
-            .then(function(subscription) {
-                // Enable any UI which subscribes / unsubscribes from
-                // push messages.
-                var pushButton = document.querySelector('.js-push-button');
-                pushButton.disabled = false;
-
-                if (!subscription) {
-                    // We aren't subscribed to push, so set UI
-                    // to allow the user to enable push
-                    return;
-                }
-
-                // Keep your server in sync with the latest subscriptionId
-                sendSubscriptionToServer(subscription);
-
-                // Set your UI to show they have subscribed for
-                // push messages
-                pushButton.textContent = 'Disable Push Messages';
-                isPushEnabled = true;
-            })
-            .catch(function(err) {
-                console.warn('Error during getSubscription()', err);
-            });
-        });
-    },
-
-    sampleControllerAction: function(cmp) {
-        // subscribe to severity levels
-        $A.logger.subscribe("INFO", logCustom);
-        // Following subscriptions not exercised here but shown for completeness
-        $A.logger.subscribe("WARNING", logCustom);
-        $A.logger.subscribe("ASSERT", logCustom);
-        $A.logger.subscribe("ERROR", logCustom);
-
-
-        function logCustom(level, message, error) {
-            console.log(getTimestamp(), "logCustom: ", arguments);
-        }
-
-        function getTimestamp() {
-            return new Date().toJSON();
-        }
     }
-
 
 })
