16f22b8bed0618064a6311c0caf594c119c76191
Rowan Christmas <rowanxmas@gmail.com>
2016-06-22 15:27:09 -0700

    dead code

--- CurrentActions/metadata/aura/InsightList/InsightListController.js
+++ CurrentActions/metadata/aura/InsightList/InsightListController.js
@@ -4,81 +4,13 @@
 
         //Fetch the insight list from the Apex controller
         helper.getInsightList(component);
-        //helper.registerServiceWorker();
-        helper.sampleControllerAction(component);
-
-        var data_action = component.get("c.getInsightCounts");
-        data_action.setCallback(this, function(actionResult) {
-
-            var result = JSON.parse(actionResult.getReturnValue());
-
-            component.set("v.global_total", result.all_users.total);
-            component.set("v.global_new_changed", result.all_users.changed+result.all_users.new);
-            component.set("v.user_total", result.user.total);
-            component.set("v.user_new_changed", result.user.changed+result.user.new);
-
-        });
-        $A.enqueueAction(data_action);
-
-        // var max_action = component.get("c.getMaxUpdateCount");
-        // max_action.setCallback(this, function(actionResult) {
-        //     component.set("v.max_updated", actionResult.getReturnValue());
-        // });
-        // $A.enqueueAction(max_action);
 
     },
 
     postScript: function(component, event, helper) {
 
-        // // init the accordion
-        // console.log('are we crashing on init?');
-        // var h = helper;
-        // var c = component;
-        // $(document).ready(function() {
-        //     $(function() {
-        //         $("#ticket-event-list").accordion({
-        //             header: '.event',
-        //             //animate : false,
-        //             //collapsible: true,
-        //             active: false,
-        //             heightStyle: "content",
-        //             beforeActivate: function( ui_event, ui ) {
-
-        //                 //   var index = ui_event.target.index;
-        //                 // debugger;
-
-        //                 // self.clickInsight(component, event, helper, index);
-        //                 // defer loading
-        //                 // console.log(event);
-        //                 // console.log(ui);
-        //                 ui.oldHeader.toggleClass('active');
-        //                 ui.newHeader.toggleClass('active');
-        //                 //helper.markUnread(component, event, helper);
-        //             },
-        //             activate: function( event, ui ) {
-        //                 var index = ui.newHeader[0].id.substring(5);
-        //                 h.clickInsight(c, index);
-        //             }
-        //         });
-        //     });
-        // });
 
     },
-
-
-    handleValueChange : function (component, event, helper) {
-
-    //  if($) {
-    //     $("#ticket-event-list").accordion({
-    //         active: false,
-    //         collapsible: true
-    //     });
-    //     console.log('are we crashing on value change?');
-    //     setTimeout(function () {
-    //         $("#ticket-event-list").accordion("refresh");
-    //     }, 0);
-    // }
-},
 
 openModal : function(component, event, helper) {
     $A.createComponent(
