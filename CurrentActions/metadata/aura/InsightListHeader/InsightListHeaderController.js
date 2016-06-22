dc9ed82a6c257eca2348a117d64cedee3ac03058
Rowan Christmas <rowanxmas@gmail.com>
2016-06-22 15:27:47 -0700

    don't listen for stuff we aren't using

--- CurrentActions/metadata/aura/InsightListHeader/InsightListHeaderController.js
+++ CurrentActions/metadata/aura/InsightListHeader/InsightListHeaderController.js
@@ -2,43 +2,31 @@
 
 	doInit : function(component, event, helper) {
 
-       var action = component.get("c.getUserName");
 
         //Set up the callback
         var self = this;
-        action.setCallback(this, function(actionResult) {
+        var name_action = component.get("c.getUserName");
+        name_action.setCallback(this, function(actionResult) {
 
-            var result = actionResult.getReturnValue();
-            component.set("v.name", result);
-
-        });
-        $A.enqueueAction(action);
-
-        var data_action = component.get("c.getInsightCounts");
-        data_action.setCallback(this, function(actionResult) {
-
-            var result = JSON.parse(actionResult.getReturnValue());
-
-            component.set("v.global_total", result.all_users.total);
-            component.set("v.global_new_changed", result.all_users.changed+result.all_users.new);
-            component.set("v.user_total", result.user.total);
-            component.set("v.user_new_changed", result.user.changed+result.user.new);
+        	var result = actionResult.getReturnValue();
+        	component.set("v.name", result);
 
         });
-        $A.enqueueAction(data_action);
+        $A.enqueueAction(name_action);
 
-        //debugger;
-     //  _.defer(function () {$('#search-select').dropdown();});
+    },
+
+    postScript : function(component, event, helper) {
 
     },
 
     assocSearch  : function(component, event, helper) {
 
-      var title = event.getParam("title");
-      component.set("v.title", title);
-  },
+    	var title = event.getParam("title");
+    	component.set("v.title", title);
+    },
 
-  menu : function(component, event, helper) {
+    menu : function(component, event, helper) {
         //console.log(event.target);
         $("#nav-dropdown").toggleClass("slds-is-open");
     }
