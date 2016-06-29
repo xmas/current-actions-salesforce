cb0f8e6cadcb4680cd5ba757185bd044a5405872
Rowan Christmas <rowanxmas@gmail.com>
2016-06-29 00:36:21 -0700

    get the Salesforce Callback URL for all to use

--- CurrentActions/metadata/aura/RelatedList/RelatedListController.js
+++ CurrentActions/metadata/aura/RelatedList/RelatedListController.js
@@ -4,12 +4,25 @@
 		var insight = component.get("v.insight");
 		var related = JSON.parse(insight.Related__c);
 
-		component.set("v.related", related);
+			//debugger;
 
+			try {
+				var self = this;
+				var url_action = component.get("c.baseURL");
+				url_action.setCallback(self, function(actionResult) {
+
+					var result = actionResult.getReturnValue();
+					component.set("v.baseURL", result);
+					component.set("v.related", related);
+				});
+				$A.enqueueAction(url_action);
+			} catch (err) {
+				console.log(err);
+				console.log(err.stack);
+			}
 		//debugger;
 
 	}
-
 
 
 })
