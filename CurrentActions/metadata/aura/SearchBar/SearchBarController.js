2d8b5580bf8d9ead5df483c0bd3af0e9e2025495
Rowan Christmas <rowanxmas@gmail.com>
2016-07-01 10:17:02 -0700

    remove unused components

--- CurrentActions/metadata/aura/SearchBar/SearchBarController.js
+++
@@ -1,9 +0,0 @@
-({
-	searchKeyChange: function(component, event, helper) {
-		var myEvent = $A.get("e.c:SearchKeyChange");
-        //console.log('fire key change: '+event.target.value);
-		myEvent.setParams({"searchKey": event.target.value});
-
-		myEvent.fire();
-	}
-})
