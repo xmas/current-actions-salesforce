eae8ae10925508546e9d528683cc581c811c36fb
Rowan Christmas <rowanxmas@gmail.com>
2016-07-03 12:22:37 -0700

    factor out select2 data refresh

--- CurrentActions/metadata/aura/InsightListHeader/InsightListHeaderHelper.js
+++ CurrentActions/metadata/aura/InsightListHeader/InsightListHeaderHelper.js
@@ -1,17 +1,29 @@
 ({
 
-    getSources : function(component) {
+    getSources : function(component, helper) {
 
         console.log('helper getReports');
         var action = component.get("c.getSources");
         //Set up the callback
         var self = this;
-        action.setCallback(this, function(actionResult) {
-            console.log('setting reports to base');
-
+        action.setCallback(self, function(actionResult) {
             component.set("v.sources", actionResult.getReturnValue());
+
         });
         $A.enqueueAction(action);
+    },
+
+    refreshFilterBox : function (component) {
+        console.log('in header controller');
+        //debugger;
+        $(".filter-select").select2().
+        on('select2:select', function (evt) {
+            console.log(evt.params.data.text);
+
+            var selectEvent = $A.get("e.c:AssocEvent");
+            selectEvent.setParams({ "assocID": evt.params.data.text });
+            selectEvent.fire();
+        });
     }
 
 })
