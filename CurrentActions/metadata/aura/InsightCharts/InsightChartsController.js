bc3a78596cf3a9a71c3acf770b563eb9e35fc056
Rowan Christmas <rowanxmas@gmail.com>
2016-07-03 12:21:27 -0700

    load data after scripts, for now

--- CurrentActions/metadata/aura/InsightCharts/InsightChartsController.js
+++ CurrentActions/metadata/aura/InsightCharts/InsightChartsController.js
@@ -2,6 +2,13 @@
 
      doInit : function(component, event, helper) {
 
+        // var insight = component.get("v.insight");
+        // helper.getDataFromS3(component, event, helper, insight);
+
+    },
+
+    postScript : function(component, event, helper) {
+
         var insight = component.get("v.insight");
         helper.getDataFromS3(component, event, helper, insight);
 
